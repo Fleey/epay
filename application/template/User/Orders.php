b2cd96aaae60b1dec1b2e5333977caabe80fed2a
Fleey-Pc <825190973@qq.com>
2019-03-10 21:42:58 +0800

    版本推送

--- application/template/User/Orders.php
+++ application/template/User/Orders.php
@@ -62,5 +62,128 @@
     </div>
 </div>
 <script>
-    $(function(){drawOrderList(1);function drawOrderList(page,searchType,searchContent){var searchData={page:page};if(searchType){searchData["type"]=searchType}if(searchContent){searchData["content"]=searchContent}$.post("/user/api/SearchOrder",searchData,function(data){$("#tips").hide();$("#orderList>tbody").html("");if(data["status"]===0){swal(data["msg"],{buttons:false,timer:1500,icon:"warning"})}else{if(data["data"].length===0){$("#tips").show().html("暂时查询不到更多数据")}$("#pagination").remove();$.each(data["data"],function(key,value){var trDom=$(document.createElement("tr"));var span1=$(document.createElement("span")).text(value["tradeNo"]);var span2=$(document.createElement("span")).text(value["tradeNoOut"]);var tdDom1=$(document.createElement("td")).append(span1).append("<br>").append(span2);var tdDom2=$(document.createElement("td")).text(value["productName"]).addClass("text-truncate ");var tdDom3=$(document.createElement("td")).text(value["money"]/100);var tdDom4;switch(value["type"]){case 1:tdDom4=$(document.createElement("td")).text("微信支付");break;case 2:tdDom4=$(document.createElement("td")).text("财付通支付");break;case 3:tdDom4=$(document.createElement("td")).text("支付宝支付");break;default:tdDom4=$(document.createElement("td")).text("未知方式");break}var tempHtml=value["createTime"]+"<br>"+(value["endTime"]?value["endTime"]:"");var tdDom5=$(document.createElement("td")).html(tempHtml);var tdDom6;if(value["status"]){tdDom6=$(document.createElement("td")).text("已支付").addClass("text-success")}else{tdDom6=$(document.createElement("td")).text("未支付").addClass("text-danger")}trDom.append(tdDom1).append(tdDom2).append(tdDom3).append(tdDom4).append(tdDom5).append(tdDom6).append('<td><button data-type="Notified" class="btn btn-outline-primary btn-sm">重新通知</button></td>');$("#orderList>tbody").append(trDom)});$("table button[data-type]").click(function(){var clickType=$(this).attr("data-type");var tradeNo=$(this).parent().parent().find(":nth-child(1)>:nth-child(1)").text();if(tradeNo.length===0){return true}if(clickType==="Notified"){$.ajax({url:"/user/api/Notified",type:"post",async:false,data:{tradeNo:tradeNo},success:function(data){if(data["status"]===0){swal(data["msg"],{buttons:false,timer:1500,icon:"warning"});return true}window.open(data["url"])}})}});$("#orderList").after('<nav id="pagination" aria-label="Page navigation" class="pagination"></nav>');$("#pagination").html("").Pagination({minPage:1,maxPage:data["totalPage"],nowPage:page,click_event:clickEvent})}},"json")}function clickEvent(nowPage,ele){var page=parseInt($(ele).text());if(ele.is(".disabled")||ele.is(".break")||ele.is(".active")){return}if(isNaN(page)){if(ele.is(".previous")){page=nowPage-1}if(ele.is(".next")){page=nowPage+1}}var content=$("#content").val();var type=$("#type").val();if(content.length!==0){drawOrderList(page,type,content)}else{drawOrderList(page)}}$("#search").click(function(){var content=$("#content").val();var type=$("#type").val();if(content.length!==0){drawOrderList(1,type,content)}else{drawOrderList(1)}})});
+    $(function () {
+        drawOrderList(1);
+
+        function drawOrderList(page, searchType, searchContent) {
+            var searchData = {page: page};
+            if (searchType) {
+                searchData["type"] = searchType
+            }
+            if (searchContent) {
+                searchData["content"] = searchContent
+            }
+            $.post("/user/api/SearchOrder", searchData, function (data) {
+                $("#tips").hide();
+                $("#orderList>tbody").html("");
+                if (data["status"] === 0) {
+                    swal(data["msg"], {buttons: false, timer: 1500, icon: "warning"})
+                } else {
+                    if (data["data"].length === 0) {
+                        $("#tips").show().html("暂时查询不到更多数据")
+                    }
+                    $("#pagination").remove();
+                    $.each(data["data"], function (key, value) {
+                        var trDom = $(document.createElement("tr"));
+                        var span1 = $(document.createElement("span")).text(value["tradeNo"]);
+                        var span2 = $(document.createElement("span")).text(value["tradeNoOut"]);
+                        var tdDom1 = $(document.createElement("td")).append(span1).append("<br>").append(span2);
+                        var tdDom2 = $(document.createElement("td")).text(value["productName"]).addClass("text-truncate ");
+                        var tdDom3 = $(document.createElement("td")).text(value["money"] / 100);
+                        var tdDom4;
+                        switch (value["type"]) {
+                            case 1:
+                                tdDom4 = $(document.createElement("td")).text("微信支付");
+                                break;
+                            case 2:
+                                tdDom4 = $(document.createElement("td")).text("财付通支付");
+                                break;
+                            case 3:
+                                tdDom4 = $(document.createElement("td")).text("支付宝支付");
+                                break;
+                            default:
+                                tdDom4 = $(document.createElement("td")).text("未知方式");
+                                break
+                        }
+                        var tempHtml = value["createTime"] + "<br>" + (value["endTime"] ? value["endTime"] : "");
+                        var tdDom5 = $(document.createElement("td")).html(tempHtml);
+                        var tdDom6;
+                        if (value["status"]) {
+                            tdDom6 = $(document.createElement("td")).text("已支付").addClass("text-success")
+                        } else {
+                            tdDom6 = $(document.createElement("td")).text("未支付").addClass("text-danger")
+                        }
+                        trDom.append(tdDom1).append(tdDom2).append(tdDom3).append(tdDom4).append(tdDom5).append(tdDom6).append('<td><button data-type="Notified" class="btn btn-outline-primary btn-sm">重新通知</button></td>');
+                        $("#orderList>tbody").append(trDom)
+                    });
+                    $("table button[data-type]").click(function () {
+                        var clickType = $(this).attr("data-type");
+                        var tradeNo = $(this).parent().parent().find(":nth-child(1)>:nth-child(1)").text();
+                        if (tradeNo.length === 0) {
+                            return true
+                        }
+                        if (clickType === "Notified") {
+                            $.ajax({
+                                url: "/user/api/Notified",
+                                type: "post",
+                                async: false,
+                                data: {tradeNo: tradeNo},
+                                success: function (data) {
+                                    if (data["status"] === 0) {
+                                        swal(data['msg'], {
+                                            buttons: false,
+                                            timer: 1500,
+                                            icon: 'error'
+                                        });
+                                        return true
+                                    }
+                                    var windows = window.open('_blank');
+                                    windows.location = data["url"];
+                                }
+                            })
+                        }
+                    });
+                    $("#orderList").after('<nav id="pagination" aria-label="Page navigation" class="pagination"></nav>');
+                    $("#pagination").html("").Pagination({
+                        minPage: 1,
+                        maxPage: data["totalPage"],
+                        nowPage: page,
+                        click_event: clickEvent
+                    })
+                }
+            }, "json")
+        }
+
+        function clickEvent(nowPage, ele) {
+            var page = parseInt($(ele).text());
+            if (ele.is(".disabled") || ele.is(".break") || ele.is(".active")) {
+                return
+            }
+            if (isNaN(page)) {
+                if (ele.is(".previous")) {
+                    page = nowPage - 1
+                }
+                if (ele.is(".next")) {
+                    page = nowPage + 1
+                }
+            }
+            var content = $("#content").val();
+            var type = $("#type").val();
+            if (content.length !== 0) {
+                drawOrderList(page, type, content)
+            } else {
+                drawOrderList(page)
+            }
+        }
+
+        $("#search").click(function () {
+            var content = $("#content").val();
+            var type = $("#type").val();
+            if (content.length !== 0) {
+                drawOrderList(1, type, content)
+            } else {
+                drawOrderList(1)
+            }
+        })
+    });
 </script>

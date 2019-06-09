d401d226c752a7fe8b25043c32a83a420894efb2
Fleey <825190973@qq.com>
2019-06-09 21:42:33 +0800

    update

--- application/command/Test.php
+++ application/command/Test.php
@@ -21,44 +21,65 @@ class Test extends Command
 
     protected function execute(Input $input, Output $output)
     {
-        $orderList = Db::table('epay_order')->where('createTime', '>=', '2019-6-6 9:00:00')
-            ->where('createTime', '<=', '2019-6-6 11:00:00')->where([
-                'status' => 0,
-                'type'   => 2
-            ])->field('tradeNo')->select();
-
-//        $qqModel   = new QQPayModel([
-//            'mchid'  => 0,
-//            'mchkey' => ''
-//        ]);
-        $wxModel = new WxPayModel([
-            'appid' => '',
-            'mchid' => '',
-            'key'   => ''
-        ], 'h5');
-        $i       = 0;
-        foreach ($orderList as $value) {
-            $value        = $value['tradeNo'];
-            $selectResult = $wxModel->selectWxPayRecord($value);
-            if ($selectResult['return_code'] != 'SUCCESS')
-                continue;
-            if ($selectResult['result_code'] != 'SUCCESS')
-                continue;
-            if ($selectResult['trade_state'] == 'SUCCESS') {
-                $i++;
-                Db::table('epay_order')->where('tradeNo=:tradeNo', ['tradeNo' => $value])->limit(1)->update([
-                    'status'  => 1,
-                    'endTime' => getDateTime()
+        $userData = Db::table('epay_user')->field('id,rate,clearMode')->cursor();
+        foreach ($userData as $value) {
+            $uid        = $value['id'];
+            $rate       = $value['rate'] / 100;
+            $totalMoney = Db::table('epay_order')->where([
+                'uid'      => $uid,
+                'status'   => 1,
+                'isShield' => 0
+            ])->whereTime('endTime', 'today')->sum('money');
+            if ($value['clearMode'] == 0) {
+                $totalMoney  = $totalMoney * ($rate / 100) / 100;
+                $deleteMoney = Db::table('epay_user_money_log')->where('uid', $uid)->whereTime('createTime', 'today')->sum('money');
+                $deleteMoney /= 1000;
+                $balance     = ($totalMoney + $deleteMoney) * 1000;
+                Db::table('epay_user')->where('id', $uid)->limit(1)->update([
+                    'balance' => $balance
                 ]);
-                //更新订单状态
-                processOrder($value);
-                //统一处理订单
-                echo $i . ' 成功更新 => ' . $value . PHP_EOL;
             }
         }
+
+//        $orderList = Db::table('epay_order')->where('createTime', '>=', '2019-6-6 9:00:00')
+//            ->where('createTime', '<=', '2019-6-6 11:00:00')->where([
+//                'status' => 0,
+//                'type'   => 2
+//            ])->field('tradeNo')->select();
+//
+////        $qqModel   = new QQPayModel([
+////            'mchid'  => 0,
+////            'mchkey' => ''
+////        ]);
+//        $wxModel = new WxPayModel([
+//            'appid' => '',
+//            'mchid' => '',
+//            'key'   => ''
+//        ], 'h5');
+//        $i       = 0;
+//        foreach ($orderList as $value) {
+//            $value        = $value['tradeNo'];
+//            $selectResult = $wxModel->selectWxPayRecord($value);
+//            if ($selectResult['return_code'] != 'SUCCESS')
+//                continue;
+//            if ($selectResult['result_code'] != 'SUCCESS')
+//                continue;
+//            if ($selectResult['trade_state'] == 'SUCCESS') {
+//                $i++;
+//                Db::table('epay_order')->where('tradeNo=:tradeNo', ['tradeNo' => $value])->limit(1)->update([
+//                    'status'  => 1,
+//                    'endTime' => getDateTime()
+//                ]);
+//                //更新订单状态
+//                processOrder($value);
+//                //统一处理订单
+//                echo $i . ' 成功更新 => ' . $value . PHP_EOL;
+//            }
     }
 
-    private function dumpSettleResult()
+
+    private
+    function dumpSettleResult()
     {
         $userList = Db::table('epay_user')->field('id,username,rate')->cursor();
         $a        = 0;
@@ -115,7 +136,8 @@ class Test extends Command
         // 指令输出
     }
 
-    private function buildCallBackUrlA(string $tradeNo, string $type)
+    private
+    function buildCallBackUrlA(string $tradeNo, string $type)
     {
         $type = strtolower($type);
         if ($type != 'notify' && $type != 'return')
@@ -149,7 +171,8 @@ class Test extends Command
         return $callBackUrl;
     }
 
-    protected function curl($url = '', $addHeaders = [], $requestType = 'get', $requestData = '', $postType = '', $urlEncode = true)
+    protected
+    function curl($url = '', $addHeaders = [], $requestType = 'get', $requestData = '', $postType = '', $urlEncode = true)
     {
         if (empty($url))
             return '';

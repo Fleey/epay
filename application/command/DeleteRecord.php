aad60af2c85469059253bc47225445f3f98bd46c
Fleey <825190973@qq.com>
2019-09-01 16:16:50 +0800

    function:增加删除规则

--- application/command/DeleteRecord.php
+++ application/command/DeleteRecord.php
@@ -22,6 +22,11 @@ class DeleteRecord extends Command
         // 指令输出
         $deleteTime = '- 15 day';
         //delete 15 day before data
+        $data = Db::table('epay_order')->whereTime('createTime', '<=', '- 3 day')->where('status', 0)->field('tradeNo')->cursor();
+        foreach ($data as $content) {
+            Db::table('epay_order_attr')->where('tradeNo', $content['tradeNo'])->delete();
+        }
+        Db::table('epay_order')->whereTime('createTime', '<=', '- 3 day')->where('status', 0)->delete();
         Db::table('epay_order')->whereTime('createTime', '<=', $deleteTime)->delete();
         Db::table('epay_order_attr')->whereTime('createTime', '<=', $deleteTime)->delete();
         Db::table('epay_settle')->whereTime('createTime', '<=', $deleteTime)->delete();
